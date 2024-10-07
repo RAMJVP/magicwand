92bd27c4a468842dbf2547c2ecab13f13db77ee3
Jeevan Patel <jeevan.v.patel@gmail.com>
2024-10-08 00:11:27 +0530

    netlify1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+
+import { useEffect, useState } from "react";
 import Message from "./Message";
 import AddBlog from "./AddBlog";
 import BlogList from "./BlogList";
